34e6d7734e538d4372e25112f135c62028a6394a
bobby <bogoblin@gmail.com>
2024-05-16 16:09:34 +0100

    Socket messages working

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::net::SocketAddr;
 
 use axum::Router;
 use futures_util::FutureExt;
+use serde_json::Value;
+use socketioxide::extract::{Bin, Data, SocketRef};
+use socketioxide::SocketIo;
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
@@ -9,8 +12,16 @@ mod world;
 
 #[tokio::main]
 async fn main() {
+    let (socket_layer, io) = SocketIo::new_layer();
+    io.ns("/", |socket: SocketRef, Data(data): Data<Value>| {
+        socket.on("click", |socket_ref: SocketRef, Data::<Value>(data), Bin(bin)| {
+            println!("Received event: {:?} {:?}", data, bin);
+        })
+    });
+
     let router: Router<> = Router::new()
-        .fallback_service(ServeDir::new("static"));
+        .fallback_service(ServeDir::new("static"))
+        .layer(socket_layer);
     let addr = SocketAddr::from(([127,0,0,1], 8000));
     let tcp = TcpListener::bind(&addr).await.unwrap();
 
