0768bb8649fe998a4f42f47591b2360fd8782922
bobby <bogoblin@gmail.com>
2024-05-18 19:45:35 +0100

    Draw a triangle with webgl

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -1,24 +1,31 @@
 use wasm_bindgen::prelude::*;
+use web_sys::WebGl2RenderingContext;
+use limelight::{Program, Renderer, DummyBuffer, DrawMode};
 
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 fn main() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    let canvas = document.get_element_by_id("game_canvas").expect("no canvas");
+    let canvas = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
+    let gl = canvas.get_context("webgl2")?
+        .expect("Can't get webgl2 context")
+        .dyn_into::<WebGl2RenderingContext>()?;
 
-    body.append_child(&val)?;
+    let mut program = Program::new(
+        include_str!("../shaders/shader.vert"),
+        include_str!("../shaders/shader.frag"),
+        DrawMode::Triangles
+    );
+
+    let mut renderer = Renderer::new(gl);
+
+    renderer.render(&mut program, &DummyBuffer::new(3)).expect("couldn't render");
 
     Ok(())
-}
-
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
 }
