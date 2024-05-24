a4071fe410325207b5da57ad96f69ae3fdf9584f
Bobby McCann <bobby@bogoblin.com>
2024-05-24 07:29:58 +0100

    Put shaders in its own file

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -1,4 +1,5 @@
 mod camera;
+mod shaders;
 
 use std::iter;
 use image::GenericImageView;
@@ -12,64 +13,13 @@ use winit::{
 
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
-use wgpu::{BindGroup, RenderPipeline, ShaderModuleDescriptor, ShaderSource, VertexAttribute, VertexState};
+use wgpu::{BindGroup, RenderPipeline, VertexAttribute, VertexState};
 use wgpu::util::{DeviceExt};
-use wgsl_inline::wgsl;
 use winit::dpi::PhysicalPosition;
 
 use world::{Position, Tile};
 use crate::camera::Camera;
-
-mod diffuse_shader {
-    use wgpu::{ShaderModuleDescriptor, ShaderSource};
-    wgsl_inline::wgsl!{
-        // Vertex shader
-        struct CameraUniform {
-            view_proj: mat4x4<f32>,
-        };
-        @group(1) @binding(0) // 1.
-        var<uniform> camera: CameraUniform;
-
-        struct VertexInput {
-            @location(0) position: vec3<f32>,
-            @location(1) tex_coords: vec2<f32>,
-        }
-
-        struct VertexOutput {
-            @builtin(position) clip_position: vec4<f32>,
-            @location(0) tex_coords: vec2<f32>,
-        }
-
-        @vertex
-        fn vs_main(
-            model: VertexInput,
-        ) -> VertexOutput {
-            var out: VertexOutput;
-            out.tex_coords = model.tex_coords;
-            out.clip_position = camera.view_proj * vec4<f32>(model.position, 1.0);
-            return out;
-        }
-
-        // Fragment shader
-
-        @group(0) @binding(0)
-        var t_diffuse: texture_2d<f32>;
-        @group(0) @binding(1)
-        var s_diffuse: sampler;
-
-        @fragment
-        fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-            return textureSample(t_diffuse, s_diffuse, in.tex_coords);
-        }
-    }
-
-    pub fn module_descriptor() -> ShaderModuleDescriptor<'static> {
-        ShaderModuleDescriptor {
-            label: None,
-            source: ShaderSource::Wgsl(SOURCE.into())
-        }
-    }
-}
+use crate::shaders::diffuse_shader;
 
 struct State<'a> {
     surface: wgpu::Surface<'a>,
