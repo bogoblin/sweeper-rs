e66b1882214b8f1baaa9aa22f209dbff865acd6e
bobby <bogoblin@gmail.com>
2024-07-10 23:19:13 +0100

    Starting to add loading indicator

--- static/Tile.js
+++ static/Tile.js
@@ -4,26 +4,12 @@ const AdjacencyMask = 0b1111; // bits 0-3 = number of adjacent mines
 const Mine = 1 << 4; // bit 4 = is there a mine?
 const Flag = 1 << 5; // bit 5 = is this flagged?
 const Revealed = 1 << 6; // bit 6 = is this revealed?
+const Loading = 1 << 7; // bit 7 = is this loading?
 
 const adjacent = tile => tile & AdjacencyMask;
 const mine = tile => (tile & Mine) !== 0;
 const flag = tile => (tile & Flag) !== 0;
 const revealed = tile => (tile & Revealed) !== 0;
-
-const PublicIfNotRevealed = Revealed | Flag;
-
-/**
- *
- * @param tile {number} The tile to decode
- */
-const tileInfo = (tile) => {
-    return {
-        revealed: revealed(tile),
-        flag: flag(tile),
-        mine: mine(tile),
-        adjacent: adjacent(tile)
-    };
-}
 
 function withFlag(tile) {
     if (flag(tile)) {
@@ -38,13 +24,5 @@ function withoutFlag(tile) {
         return tile - Flag;
     } else {
         return tile;
-    }
-}
-
-const publicVersion = (tile) => {
-    if (revealed(tile)) {
-        return tile;
-    } else {
-        return tile & PublicIfNotRevealed;
     }
 }
