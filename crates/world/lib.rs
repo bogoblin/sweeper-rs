dad12d42444e5d3022aafddb63b6e6eacd3bd80a
bobby <bogoblin@gmail.com>
2024-09-19 15:32:36 +0100

    A bit more of an elegant solution

--- crates/world/lib.rs
+++ crates/world/lib.rs
@@ -527,10 +527,15 @@ impl UpdatedRect {
             // It crashed here at the 2 billion boundary because it was trying to make a huge rect.
             // This is possible to fix with a wrapping boundary, which I haven't implemented just yet.
             // Maybe we can configure the boundary size hmm
-            if x > 1000 || y > 1000 || x < 0 || y < 0 {
-                return Self::empty()
+            let x = x as usize;
+            let y = y as usize;
+            if let Some(col) = updated.get_mut(x) {
+                if col.len() > y {
+                    col.insert(y, updated_tile.tile);
+                    continue;
+                }
             }
-            updated[x as usize][y as usize] = updated_tile.tile;
+            return Self::empty();
         }
 
         Self {
