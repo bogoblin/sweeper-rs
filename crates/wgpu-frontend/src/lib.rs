315e24b07e3ab8afdc90abeecdf7b53c0fe0a3a4
Bobby McCann <bogoblin@gmail.com>
2024-12-02 12:56:05 +0000

    Index buffer for purple polygon

--- crates/wgpu-frontend/src/lib.rs
+++ crates/wgpu-frontend/src/lib.rs
@@ -96,6 +96,8 @@ struct State<'a> {
     window: &'a Window,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 
     mouse: MouseState,
 }
@@ -204,6 +206,14 @@ impl<'a> State<'a> {
                 usage: wgpu::BufferUsages::VERTEX,
             }
         );
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indices = INDICES.len() as u32;
 
         Self {
             window,
@@ -214,6 +224,8 @@ impl<'a> State<'a> {
             size,
             render_pipeline,
             vertex_buffer,
+            index_buffer,
+            num_indices,
             mouse: MouseState::new(),
         }
     }
@@ -273,7 +285,8 @@ impl<'a> State<'a> {
 
             _render_pass.set_pipeline(&self.render_pipeline);
             _render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            _render_pass.draw(0..VERTICES.len() as u32, 0..1);
+            _render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            _render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
         self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
@@ -300,10 +313,19 @@ struct Vertex {
     position: [f32; 3],
     color: [f32; 3],
 }
+
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
 ];
 
 impl Vertex {
