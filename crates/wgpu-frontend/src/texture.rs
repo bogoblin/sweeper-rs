9b93a63ac931f0c4c41489dbf39f5191b923e370
Bobby McCann <bogoblin@gmail.com>
2024-12-04 15:47:47 +0000

    Mipmap on the tiles

--- crates/wgpu-frontend/src/texture.rs
+++ crates/wgpu-frontend/src/texture.rs
@@ -1,4 +1,5 @@
-use image::GenericImageView;
+use image::imageops::FilterType;
+use image::{GenericImageView};
 use anyhow::*;
 
 pub struct Texture {
@@ -25,18 +26,16 @@ impl Texture {
         img: &image::DynamicImage,
         label: Option<&str>,
     ) -> Result<Self> {
-        let rgba = img.to_rgba8();
-        let dimensions = img.dimensions();
-
-        let size = wgpu::Extent3d {
-            width: dimensions.0,
-            height: dimensions.1,
-            depth_or_array_layers: 1,
-        };
+        let mip_level_count = img.height().ilog2() + 1;
+        // let mip_level_count = 1;
         let texture = device.create_texture(&wgpu::TextureDescriptor {
             label,
-            size,
-            mip_level_count: 1,
+            size: wgpu::Extent3d {
+                width: img.width(),
+                height: img.height(),
+                depth_or_array_layers: 1,
+            },
+            mip_level_count,
             sample_count: 1,
             dimension: wgpu::TextureDimension::D2,
             format: wgpu::TextureFormat::Rgba8UnormSrgb,
@@ -44,31 +43,45 @@ impl Texture {
             view_formats: &[],
         });
 
-        queue.write_texture(
-            wgpu::ImageCopyTexture {
-                aspect: wgpu::TextureAspect::All,
-                texture: &texture,
-                mip_level: 0,
-                origin: wgpu::Origin3d::ZERO,
-            },
-            &rgba,
-            wgpu::ImageDataLayout {
-                offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
-            },
-            size,
-        );
+        for mip_level in 0..mip_level_count {
+            let scale = 1 << mip_level;
+            let mip = img.resize(
+                img.width().div_ceil(scale),
+                img.height().div_ceil(scale),
+                FilterType::Gaussian,
+            );
+            let (width, height) = mip.dimensions();
+            println!("{:?}", mip.dimensions());
+            queue.write_texture(
+                wgpu::ImageCopyTexture {
+                    aspect: wgpu::TextureAspect::All,
+                    texture: &texture,
+                    mip_level,
+                    origin: wgpu::Origin3d::ZERO,
+                },
+                &mip.into_rgba8(),
+                wgpu::ImageDataLayout {
+                    offset: 0,
+                    bytes_per_row: Some(4 * width),
+                    rows_per_image: Some(height),
+                },
+                wgpu::Extent3d {
+                    width,
+                    height,
+                    depth_or_array_layers: 1,
+                }
+            );
+        }
 
         let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
         let sampler = device.create_sampler(
             &wgpu::SamplerDescriptor {
-                address_mode_u: wgpu::AddressMode::Repeat,
-                address_mode_v: wgpu::AddressMode::Repeat,
-                address_mode_w: wgpu::AddressMode::Repeat,
+                address_mode_u: wgpu::AddressMode::ClampToEdge,
+                address_mode_v: wgpu::AddressMode::ClampToEdge,
+                address_mode_w: wgpu::AddressMode::ClampToEdge,
                 mag_filter: wgpu::FilterMode::Nearest,
                 min_filter: wgpu::FilterMode::Nearest,
-                mipmap_filter: wgpu::FilterMode::Nearest,
+                mipmap_filter: wgpu::FilterMode::Linear,
                 ..Default::default()
             }
         );
