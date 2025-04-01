456f93ed6248f004213093e2b453bf9919d98209
bobby <bogoblin@gmail.com>
2025-04-01 10:44:28 +0100

    Fix event equality before/after serialization
    I was conflating clicks and double clicks, which was fine, but it was messing with this test

--- crates/world/updates/events.rs
+++ crates/world/updates/events.rs
@@ -4,7 +4,8 @@ use std::i32;
 use quickcheck::{Arbitrary, Gen};
 use crate::player::Player;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone)]
+#[derive(PartialEq)]
 #[derive(Serialize, Deserialize)]
 pub enum Event {
     Clicked {
@@ -71,15 +72,17 @@ impl Event {
     pub fn compress(&self) -> Vec<u8> {
         let mut binary = vec![];
         let (header, player_id, at, updated) = match self {
-            Event::Clicked { player_id, at, updated } |
+            Event::Clicked { player_id, at, updated } => {
+                ("C", player_id, at, Some(updated))
+            }
             Event::DoubleClicked { player_id, at, updated } => {
-                ("c", player_id, at, Some(updated))
+                ("D", player_id, at, Some(updated))
             }
             Event::Flag { player_id, at } => {
-                ("f", player_id, at, None)
+                ("F", player_id, at, None)
             }
             Event::Unflag { player_id, at } => {
-                ("u", player_id, at, None)
+                ("U", player_id, at, None)
             }
         };
 
@@ -112,28 +115,21 @@ impl Event {
             index += 8;
             Position(x, y)
         };
-        
-        if header == "c" {
-            match UpdatedRect::from_compressed(&compressed[index..]) {
-                None => None,
-                Some(updated) => {
-                    Some(Event::Clicked {
-                        player_id,
-                        at,
-                        updated
-                    })
-                }
-            }
-        } else if header == "f" {
-            Some(Event::Flag {
-                player_id,
-                at
-            })
-        } else if header == "u" {
-            Some(Event::Unflag {
-                player_id,
-                at
-            })
+
+        if header == "C" {
+            UpdatedRect::from_compressed(&compressed[index..])
+                .map(|updated| {
+                    Event::Clicked { player_id, at, updated }
+                })
+        } else if header == "D" {
+            UpdatedRect::from_compressed(&compressed[index..])
+                .map(|updated| {
+                    Event::DoubleClicked { player_id, at, updated }
+                })
+        } else if header == "F" {
+            Some(Event::Flag { player_id, at })
+        } else if header == "U" {
+            Some(Event::Unflag { player_id, at })
         } else {
             None
         }
@@ -169,49 +165,8 @@ mod tests {
     quickcheck! {
         fn event_compression_then_decompression(event: Event) -> bool {
             let compressed = event.compress();
-            let decompressed = Event::from_compressed(&*compressed);
-            event.updated_rect() == decompressed.unwrap().updated_rect()
+            let decompressed = Event::from_compressed(&*compressed).unwrap();
+            event == decompressed
         }
     }
-}
-
-#[test]
-fn compression_test() -> Result<(), String> {
-    let p = "Gary";
-    let pos = Position(-50, 300);
-    let flag = Event::Flag {
-        player_id: p.to_string(),
-        at: pos
-    };
-    let decompressed = Event::from_compressed(&*flag.compress()).ok_or("uh oh")?;
-    match decompressed {
-        Event::Flag { player_id, at } => {
-            assert_eq!(player_id, p);
-            assert_eq!(at, pos);
-        }
-        _ => panic!("Wrong event type")
-    }
-
-    let to_update = vec![
-        UpdatedTile { position: Position(1, 1), tile: Tile::empty().with_revealed() },
-        UpdatedTile { position: Position(3, 1), tile: Tile::empty().with_revealed() },
-        UpdatedTile { position: Position(3, 3), tile: Tile::mine().with_revealed() },
-        UpdatedTile { position: Position(2, 3), tile: Tile::mine().with_flag() },
-    ];
-    let clicked = Event::Clicked {
-        player_id: p.to_string(),
-        at: pos,
-        updated: UpdatedRect::new(to_update.clone())
-    };
-    let decompressed = Event::from_compressed(&*clicked.compress()).ok_or("uh oh")?;
-    match decompressed {
-        Event::Clicked { player_id, at, updated } => {
-            assert_eq!(player_id, p);
-            assert_eq!(at, pos);
-            assert_eq!(updated.public_tiles(), UpdatedRect::new(to_update).public_tiles());
-        }
-        _ => panic!("Wrong event type")
-    }
-
-    Ok(())
 }
